9390ba81f9bee65e5c7f2f31fb8aa0172b3f95ce
Ido Sadeh <idosadehrosenfeld@gmail.com>
2020-05-14 20:29:30 +0300

    added svg's to the 'how it works' texts

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about_us')
+def about_us():
+    return render_template("about_us.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
