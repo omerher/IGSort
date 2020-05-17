d78f62babef41d8b9d6b4367b2ad2c7d4ba033e8
Ido Sadeh <idosadehrosenfeld@gmail.com>
2020-05-17 17:37:47 +0300

    added flash function to 'More information' section

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,11 @@ const usernameInput = document.querySelector('#anayzeFormUsernameInput');
 const postsInput = document.querySelector('#analyzeFormNumberInput');
 const getPostsButton = document.querySelector('#getPostsButton');
 const loadingButton = document.querySelector('#loadingButton');
+const moreInfoButton = document.querySelector('#more-info-button');
+const moreInfoBox = document.querySelector('#more-info-box');
+const homeButton = document.querySelector('home-button');
+var onHome = true;
+
 
 getPostsButton.addEventListener('click', () => {
   getPostsButton.style.display = 'none';
@@ -9,4 +14,24 @@ getPostsButton.addEventListener('click', () => {
 
   usernameInput.setAttribute('readonly', 'true');
   postsInput.setAttribute('readonly', 'true');
+  homeButton.classList.remove("active");
+  onHome = false;
 })
+
+
+
+moreInfoButton.addEventListener('click', () => {
+  if(onHome){
+      moreInfoBox.style.background = 'rgba(217, 30, 24, 0.15)';
+      setTimeout(() => {moreInfoBox.style.background = 'none'} , 1500);
+  }
+  else{
+    window.location.replace("{{ url_for('index') }}");
+    onHome = true;
+  }
+  
+})
+
+homeButton.addEventListener('click', homeButton.classList.add("active"));
+
+
